c0b69894c50e19d652a9ae2ea8576d64f889daa5
Jan Kobler <eng1@koblersystems.de>
2014-12-17 14:05:31 +0100

    configuration improved
    --sync
    --debug
    --password
    --sendwait
    
    Signed-off-by: Jan Kobler <eng1@koblersystems.de>

--- src/de/koblersystems/mpc55xx/mpc55xx_bam_01.py
+++ src/de/koblersystems/mpc55xx/mpc55xx_bam_01.py
@@ -37,6 +37,8 @@ import os
 import argparse
 import binascii
 
+import time
+
 class bam_01:
     
     def __init__(self):
@@ -49,6 +51,10 @@ class bam_01:
         self.sram_start1 = 0x40000000
         self.flag_test3 = False
         self.file_name1 = "test.bin"
+        self.ser_sync1 = False
+        self.flag_debug1 = False
+        self.password1 = "FEED FACE CAFE BEEF"
+        self.send_wait1 = None # Time in seconds, float
     
     def test_01(self):
         
@@ -56,11 +62,14 @@ class bam_01:
             self.ser1 = serial.Serial(self.ser_port1, 9600, timeout=30)
             self.pos1 = 0
             
-        # serial boot password
-        pw1 = bytes.fromhex("FEED FACE CAFE BEEF")
-        print("Password", pw1)
+        pw1 = self.get_password1()
+        
+        self.send_sync1()
+        
+        self.show_send_wait1()
         
         # write password
+        print("Password", pw1)
         self.write_01(pw1)
  
         sram1 = (self.load_address1,self.sram_size1) # start, len
@@ -83,11 +92,11 @@ class bam_01:
             
             while self.pos1 < image1[1]:
                 self.write_01((0x00).to_bytes(1,'big'))
-        else: 
-            if self.flag_test1 != "echo":
-                print("unknown test type", self.flag_test1, ",use test echo instead", file=sys.stderr)
+        elif self.flag_test1 == "echo": 
             for d1 in range(image1[1]//4):
                 self.write_01(d1.to_bytes(4,'big'))
+        else:
+            print("unknown test type", self.flag_test1, file=sys.stderr)
             
         print("Done")
 
@@ -125,12 +134,15 @@ class bam_01:
         if True:
             self.ser1 = serial.Serial(self.ser_port1, 9600, timeout=30)
             self.pos1 = 0
-            
-        # serial boot password
-        pw1 = bytes.fromhex("FEED FACE CAFE BEEF")
-        print("Password", pw1)
+                                
+        pw1 = self.get_password1()
+
+        self.send_sync1()
+        
+        self.show_send_wait1()
         
         # write password
+        print("Password", pw1)
         self.write_01(pw1)
  
         
@@ -189,19 +201,46 @@ class bam_01:
             raise Exception("buf1 has to be bytes")
         
         for b1 in buf1:
+            
             print("pos: ", hex(self.pos1),"write: ", hex(b1))
             if self.ser1 is not None:
+                if self.send_wait1:
+                    time.sleep(self.send_wait1)
                 self.ser1.write(b1.to_bytes(1,byteorder='big'))
                 b2 = self.ser1.read()
-                #print("read:", b2, "len:", len(b2))
+                if self.flag_debug1:
+                    print("read:", b2, "len:", len(b2))
+
                 if len(b2)>1:
                     raise Exception("more than one byte returned")
                 
-                if b2 is None:
-                    raise Exception("None returned, time out: ")
+                if b2 is None or len(b2)==0:
+                    raise Exception("Nothing returned, time out")
                 elif b2[0] != b1:
                     raise Exception("wrong character returned: " + hex(b2[0]) + " expected: " + hex(b1))
             self.pos1 += 1
+
+    def send_sync1(self):
+        
+        if self.ser_sync1 == True:
+            b1 = 0x0
+            print("MPC56xx Baud Rate detection, send 0x0 as test frame")
+            print("pos: ", hex(self.pos1),"write: ", hex(b1))
+            self.ser1.write((0x0).to_bytes(1,byteorder='big'))    
+            # No answer expected
+            self.pos1 += 1
+
+    def get_password1(self):
+        
+        # serial boot password
+        pw1 = bytes.fromhex(self.password1)
+        if len(pw1) != 8:
+            print("Warning: Password length:", len(pw1), "is wrong, required", 8, "It will be sent, but the password is definitely wrong")
+        return pw1
+    
+    def show_send_wait1(self):
+        if self.send_wait1 is not None:
+            print( "Waiting " + str(self.send_wait1) + "s before sending")
 
     def test_03(self):
         """ read output """
@@ -217,11 +256,11 @@ class bam_01:
         print("Waiting for output, speed", speed1)
         while True:
             b2 = self.ser1.read(1)
-            if b2 is None:
+            if b2 is None or len(b2) == 0:
                 raise Exception("None returned, time out: ")
             
             for b1 in b2:
-                print("Returned: " + hex(b1) + " '" + str(b1) + "'")
+                print("Returned: " + hex(b1) + " '" + str(b1) + "' " + chr(b1) )
        
     def parse_args_01(self, test_args1 = None):
         
@@ -242,6 +281,13 @@ class bam_01:
         p1.add_argument("--port", help="serial port", default=sys_port1)
         
         p1.add_argument("--listen", action="store_true", help="listen to serial port AFTER uploading, speed 115200",)
+
+        p1.add_argument("--sync", action="store_true", help="MPC56xx only: send sync byte at the beginning for Baud Rate Detection ")
+
+        p1.add_argument("--debug", action="store_true", help="provide debug output")
+        
+        p1.add_argument("--password", help="Password: 8byte, hex, spaces allowed, e.g. FEED FACE CAFE BEEF")
+        p1.add_argument("--sendwait", help="Time to wait before sending a byte, in seconds, may be float number")
         
         # parse
         a1 = p1.parse_args(test_args1)
@@ -268,7 +314,20 @@ class bam_01:
             if a1.listen:
                 print("wait for output:", a1.listen)
                 
+            if a1.sync:
+                print("Sync: ", a1.sync)
                 
+            if a1.debug:
+                print("Debug: ", a1.debug)
+                
+            if a1.password:
+                print("Password:", a1.password)
+                
+            if a1.sendwait:
+                print("Sendwait:", a1.sendwait)
+                
+                
+            print()
             # copy settings and start
             
             if a1.port:
@@ -279,6 +338,18 @@ class bam_01:
                 
             if a1.listen:
                 self.flag_test3 = a1.listen
+                
+            if a1.sync:
+                self.ser_sync1 = a1.sync
+                
+            if a1.debug:
+                self.flag_debug1 = a1.debug
+                
+            if a1.password:
+                self.password1 = a1.password
+                
+            if a1.sendwait:
+                self.send_wait1 = float(a1.sendwait)
                 
             # start 
             if a1.test:
