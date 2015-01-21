c80f4ec9a27ee79033c53cc5473be5568b166482
Jan Kobler <eng1@koblersystems.de>
2015-01-21 15:43:42 +0100

    copyright year
    Signed-off-by: Jan Kobler <eng1@koblersystems.de>

--- src/mpc55xx/mpc55xx_bam_01.py
+++ src/mpc55xx/mpc55xx_bam_01.py
@@ -1,7 +1,7 @@
 # 
 # The MIT License (MIT)
 # 
-# Copyright (c) 2014 Kobler Systems GmbH
+# Copyright (c) 2014, 2015 Kobler Systems GmbH
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
 # 
