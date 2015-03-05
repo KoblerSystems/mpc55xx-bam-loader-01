fe32a7b5d4511b317d5eb74dfb60030c73a41920
Jan Kobler <eng1@koblersystems.de>
2015-03-05 17:00:04 +0100

    update license
    Signed-off-by: Jan Kobler <eng1@koblersystems.de>

--- src/mpc55xx/mpc55xx_bam_01.py
+++ src/mpc55xx/mpc55xx_bam_01.py
@@ -1,6 +1,4 @@
 # 
-# The MIT License (MIT)
-# 
 # Copyright (c) 2014, 2015 Kobler Systems GmbH
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
@@ -20,6 +18,9 @@
 # LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# (The MIT License can be found at http://opensource.org/licenses/MIT)
+# 
 #
 # Author: Jan Kobler, Kobler Systems GmbH, email: eng1@koblersystems.de
 #
